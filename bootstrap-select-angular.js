6cd06c0a5c4a2460d4a50f4daaf2e1163011a956
unknown <tub97573@CIS-WCC20918.tu.temple.edu>
2014-04-02 19:18:14 -0400

    test

--- bootstrap-select-angular.js
+++ bootstrap-select-angular.js
@@ -14,13 +14,40 @@ angular.module('docsIsolateScopeDirective', [])
       restrict: 'E',
       scope: {
         ngModel: '=ngModel',
-        ngOptions: '=ngOptions'
+        ngOptions: '=ngOptions',
+        countSelectedText,'=countSelectedText',
+        dropupAuto: '=dropupAuto',
+        header: '=header',
+        hideDisabled: '=hideDisabled',
+        selectedTextFormat: '=selectedTextFormat',
+        size: '=size',
+        showSubtext: '=showSubtext',
+        showIcon: '=showIcon',
+        showContent: '=showContent',
+        title: '=title',
+        width: '=width',
       },
       template: 'my-customer-iso.html',
       link: function(scope, element, attrs, tabsCtrl) {
-        $('.selectpicker').selectpicker({
-          'selectedText': 'cat'
+        $(element).selectpicker({
+          countSelectedText,countSelectedText,
+          dropupAuto: dropupAuto,
+          header: header,
+          hideDisabled: hideDisabled,
+          selectedTextFormat: selectedTextFormat,
+          size: size,
+          showSubtext: showSubtext,
+          showIcon: showIcon,
+          showContent: showContent,
+          title: title,
+          width: width,        
         });
+
+        scope.$watch(attrs.myCurrentTime, function(value) {
+          format = value;
+          updateTime();
+        });
+
       },
     };
   });
