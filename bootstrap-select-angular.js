acc4fa2893087eac48450b70cee77756aa0f78eb
unknown <tub97573@CIS-WCC20918.tu.temple.edu>
2014-04-02 18:05:15 -0400

    set up the template

---
+++ bootstrap-select-angular.js
@@ -0,0 +1,20 @@
+/**
+ * Author: Yaw Asabere
+ *
+ * 
+ */
+
+angular.module('docsIsolateScopeDirective', [])
+  .controller('Controller', ['$scope', function($scope) {
+    $scope.naomi = { name: 'Naomi', address: '1600 Amphitheatre' };
+    $scope.igor = { name: 'Igor', address: '123 Somewhere' };
+  }])
+  .directive('myCustomer', function() {
+    return {
+      restrict: 'E',
+      scope: {
+        customerInfo: '=info'
+      },
+      template: 'my-customer-iso.html'
+    };
+  });
