465e497b4635c036eded7c0bd97a726f68fbfa93
Yaw Asabere <yasabere@gmail.com>
2014-04-03 16:17:29 -0400

    Update bootstrap-select-angular.js

--- bootstrap-select-angular.js
+++ bootstrap-select-angular.js
@@ -8,6 +8,11 @@ angular.module('bootstrapSelectDirective', [])
   .controller('Controller', ['$scope', '$interpolate', function($scope, $interpolate) {
     $scope.naomi = { name: 'Naomi', address: '1600 Amphitheatre' };
     $scope.igor = { name: 'Igor', address: '123 Somewhere' };
+    
+    $scope.test = function(){
+      console.log('test');
+    }
+    
   }])
   .directive('bootstrapSelect', function() {
     return {
@@ -15,6 +20,11 @@ angular.module('bootstrapSelectDirective', [])
       scope: {
         ngModel: '=ngModel',
         ngOptions: '=ngOptions',
+        ngdataType:'=ngDataType' ,
+        ngSelect:'=ngSelect' ,
+        ngLabel:'=ngLabel',
+        ngGroupBy:'=ngGroupBy',
+        
         countSelectedText:'=countSelectedText',
         dropupAuto: '=dropupAuto',
         header: '=header',
@@ -34,6 +44,15 @@ angular.module('bootstrapSelectDirective', [])
           post: function postLink(scope, iElement, iAttrs, controller) {
             
             var dropdown = $(iElement[0].childNodes[0]);
+            var val;
+            
+            scope.update = function(val){
+                val = dropdown.selectpicker('val');
+                scope.ngModel = val;
+                //alert(scope.ngModel);
+                //scope.ngModel = iElement[0].childNodes[1].childNodes[0].childNodes[0].childNodes[0].data;
+                //scope.$apply();
+              }
             
             dropdown.selectpicker({
               countSelectedText: scope.countSelectedText,
@@ -48,16 +67,33 @@ angular.module('bootstrapSelectDirective', [])
               title: scope.title,
               width: scope.width,        
               options: scope.ngOptions,
+              onSelect: scope.update
             });
             
-            //console.log(element[0].childNodes[0]);
-            
-            scope.$watch(scope.ngOptions, function(value) {
+            var refresh = function(){
               dropdown.selectpicker('refresh');
               dropdown.selectpicker('render');
+            
+              dropdown.siblings('div').eq(0).children('div').children('ul').children('li').each(function() {
+                var $this = $(this);
+                
+                console.log('this, ',$this);
+                
+                $this.click(function() {
+                  dropdown.selectpicker('val', 'black');
+                });
+                  
+              });
+              
+              scope.$apply();
+            
+            };
+            
+            scope.$watch(scope.ngOptions, function(value) {
+              refresh();
             });
             
-            //alert('test');
+            
             
           }
         };
